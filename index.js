95b8df855adaaa29c76a5d41bc41718ce0b50cc9
Den <52295780+josserden@users.noreply.github.com>
2023-02-23 16:42:11 +0200

    add: create service with the basic function

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
-
 import { getArgs } from './helpers/args.js';
+import { printHelp } from './services/log-service.js';
 
 const init = () => {
   const args = getArgs(process.argv);
-  console.log(args);
 
-  // if (args.h) {
-  //   console.log('Help');
-  // }
+  if (args.h) {
+    printHelp();
+  }
 
   // if (args.s) {
   //   console.log('Save city');
---
+++ services/log-service.js
@@ -0,0 +1,22 @@
+import chalk from 'chalk';
+import dedent from 'dedent-js';
+
+export const printError = (error) => {
+  console.log(chalk.bgRed(`ERROR: ${error}`));
+};
+
+export const printSuccess = (message) => {
+  console.log(chalk.bgGreen(`SUCCESS: ${message}`));
+};
+
+export const printHelp = () => {
+  console.log(
+    dedent`
+    ${chalk.white.bgBlue.bold('HELP')}
+    ${chalk.bold(dedent`without arguments - the program will show the weather for your current location
+    -s [CITY] - set the city
+    -t [API_KEY] - set the API key
+    -h - to show help`)}
+    `
+  );
+};
